624741bde6a12997305295d6f140941457a23d70
stm_git <stm>
2024-07-18 16:30:55 +0900

    test

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 // const BASE_URL = 'https://learn.codeit.kr/api/codestudit';
 const BASE_URL = 'http://api.kcisa.kr/openapi/service/rest/meta14/getKMPC031801?serviceKey=854f4923-0a54-4479-b114-bd82979e3fac';
 // const BASE_URL = 'http://api.kcisa.kr/openapi/service/rest/meta14/getKMPC031801?serviceKey=854f4923-0a54-4479-b114-bd82979e3fac&numOfRows=10&pageNo=1';
+//test
 
 export async function getPosts() {
   const {response} = await fetch(`${BASE_URL}`);
