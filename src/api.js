4009502acaa9d9f929f5bb48fc6aa2e0d8dc0932
TM Book <cestmoi7531@gmail.com>
2024-07-29 21:41:26 +0900

    무한 스크롤 구현

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-// const BASE_URL = 'https://learn.codeit.kr/api/codestudit';
 const API_KEY = process.env.REACT_APP_API_KEY
+
 const BASE_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`;
 
 //&page=${page} 페이지 수
 
-export async function getPosts({ pageParam = 2 }) {
+export async function getPosts({ pageParam = 1 }) {
   const response = await fetch(`${BASE_URL}&page=${pageParam}`);
   // if (!response.ok) {
   //   throw new Error('Network response was not ok');
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { useQuery } from '@tanstack/react-query';
-import { getPosts } from '../api';
+import React, { useEffect, useRef, useCallback } from "react";
+import { useInfiniteQuery  } from '@tanstack/react-query';
+import { getPosts} from '../api';
 
 import MovieItem from './MovieItem'
 
 const MovieInfo = () => {
-    const { data, isLoading, isError } = useQuery({ queryKey: ['movieList'], queryFn: getPosts });
+    const {
+        data,
+        isLoading,
+        isError,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage
+    } = useInfiniteQuery({
+        queryKey: ['movieList'],
+        queryFn: ({ pageParam = 1 }) => getPosts({ pageParam }),
+        getNextPageParam: (lastPage, allPages) => {
+            const nextPage = lastPage.page + 1;
+            return nextPage <= lastPage.total_pages ? nextPage : undefined;
+        }
+    });
+
+    const observerElem = useRef();
+
+    const lastMovieElementRef = useCallback(node => {
+        if (isLoading || isFetchingNextPage) return;
+        if (observerElem.current) observerElem.current.disconnect();
+        observerElem.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasNextPage) {
+                fetchNextPage();
+            }
+        });
+        if (node) observerElem.current.observe(node);
+    }, [isLoading, isFetchingNextPage, fetchNextPage, hasNextPage]);
+
 
     //데이터를 불러오는 동안 로딩 상태 처리 (옵셔녈 체이닝으로 가능)
     if (isLoading) {
@@ -16,18 +44,43 @@ const MovieInfo = () => {
         return <div>Error...</div>;
     }
 
-    let list = data?.results ?? [];
+    // let list = data?.results ?? [];
+    const list = data?.pages.flatMap(page => page.results) ?? [];
 
     // console.log(result.data?.results[0].release_date);
     console.log(list);
-    console.log(data?.total_pages);
 
     return (
-       <>
-        {list.map((item) => (
-            <MovieItem poster_path={item.poster_path} title={item.title} year={item.release_date} genre_ids={item.genre_ids} overview={item.overview} key={item.id}/>
-        ))}
-       </>
+        <div>
+        {list.map((item, index) => {
+            if (list.length === index + 1) {
+                return (
+                    <div ref={lastMovieElementRef} key={item.id}>
+                        <MovieItem
+                            poster_path={item.poster_path}
+                            title={item.title}
+                            year={item.release_date}
+                            genre_ids={item.genre_ids}
+                            overview={item.overview}
+                        />
+                        <p>ref 포함</p>
+                    </div>
+                );
+            } else {
+                return (
+                    <MovieItem
+                        poster_path={item.poster_path}
+                        title={item.title}
+                        year={item.release_date}
+                        genre_ids={item.genre_ids}
+                        overview={item.overview}
+                        key={item.id}
+                    />
+                );
+            }
+        })}
+        {isFetchingNextPage && <div>Loading more...</div>}
+    </div>
     );
 };
 
