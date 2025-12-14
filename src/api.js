cdde1247c4047986b5b05e8328208bf69da0c9c4
King sejong <cestmoi7531@gmail.com>
2025-12-14 20:55:31 +0900

    환경변수 설정

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API_KEY = process.env.REACT_APP_API_KEY
 
-export async function getPosts({ pageParam = 1, withGenres = '', withOriginalLanguage = '', startYear='', endYear=''}) {
+export async function getPosts({ pageParam = 1, withGenres = '', withOriginalLanguage = '', startYear = '', endYear = '' }) {
 
   const BASE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=ko-KR&with_genres=${withGenres}&with_original_language=${withOriginalLanguage}&primary_release_date.gte=${startYear}-01-01&primary_release_date.lte=${endYear}-12-31`;
 
